b3d590dacd471d6294341d78a3017111664704f3
Cheolkie Kim <ckiekim@naver.com>
2022-01-12 13:51:14 +0900

    신경망 - 회귀

--- 01.Basic/boston2.py
+++ 01.Basic/boston2.py
@@ -1,33 +1,26 @@
 # 입력으로 0에서 100사이의 정수값을 받아서
 # 보스톤 주택가격의 실제값과 예측값을 보여주는 프로그램
+# 모델을 학습하지 않고 저장된 모델 활용
 # import part
 import numpy as np
 import pandas as pd
 import warnings
-import tensorflow as tf
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import load_model
 
 # 상수값 설정 등 변수 초기화
 seed = 2022
+model_filename = 'boston.h5'
 warnings.filterwarnings('ignore')
 np.random.seed(seed)
-tf.random.set_seed(seed)
 boston = load_boston()
 X_train, X_test, y_train, y_test = train_test_split(
     boston.data, boston.target, test_size=0.1, random_state=seed
 )
 
-# 메인 모델 만들기
-model = Sequential([
-    Dense(30, input_dim=13, activation='relu'),
-    Dense(12, activation='relu'),
-    Dense(1)                        # 회귀인 경우 activation 함수를 지정하지 않음.
-])
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, validation_split=0.1, epochs=500, batch_size=60, verbose=0)
+# 저장된 메인 모델 불러오기
+model = load_model(model_filename)
 
 # 입력값 받기
 index = int(input('0 ~ 50 정수값을 입력하세요.> '))
